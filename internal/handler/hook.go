c9998e0bf34963be8a25613d16aa49b604ed3882
Blackcloudss <3444535358@qq.com>
2024-11-12 00:32:57 +0800

    feat(hook): add other id

--- internal/handler/hook.go
+++ internal/handler/hook.go
@@ -8,23 +8,63 @@ import (
 )
 
 // 默认节点
-const DefaultNodeID = 1
+const Default_User_ID = 1
+const Default_Team_ID = 2
+const Default_Node_ID = 3
 
-// RegisterHooks 注册 gorm 钩子
-func RegisterHooks(db *gorm.DB) {
-	db.Callback().Create().Before("gorm:Create").Register("before_create_hook", BeforeCreateHook)
+// 注册 gorm 钩子
+func RegisterUser(db *gorm.DB) {
+	db.Callback().Create().Before("gorm:Create").Register("before_create_User", BeforeCreateUser)
 }
 
-// 创建对象之前的钩子函数
-func BeforeCreateHook(tx *gorm.DB) {
-	node, err := snowflake.NewNode(DefaultNodeID)
+// 在操作数据库前 创建 用户 ID
+func BeforeCreateUser(tx *gorm.DB) {
+	node, err := snowflake.NewNode(Default_User_ID)
 	if err != nil {
 		zlog.Fatalf("生成 Node 出错")
 	}
 
 	tx.Statement.ReflectValue = reflect.Indirect(tx.Statement.ReflectValue)
-	// 确认 ID 字段是否存在，并且是 string 类型
-	if field := tx.Statement.ReflectValue.FieldByName("ID"); field.IsValid() && field.CanSet() {
+	// 确认 user_id 字段是否存在，并且是 string 类型
+	if field := tx.Statement.ReflectValue.FieldByName("user_id"); field.IsValid() && field.CanSet() {
+		// 设置生成的唯一 ID
+		field.SetString(node.Generate().String())
+	}
+}
+
+func RegisterTeam(db *gorm.DB) {
+	db.Callback().Create().Before("gorm:Create").Register("before_create_Team", BeforeCreateTeam)
+}
+
+// 在操作数据库前 创建 团队 ID
+func BeforeCreateTeam(tx *gorm.DB) {
+	node, err := snowflake.NewNode(Default_Team_ID)
+	if err != nil {
+		zlog.Fatalf("生成 Node 出错")
+	}
+
+	tx.Statement.ReflectValue = reflect.Indirect(tx.Statement.ReflectValue)
+	// 确认 team_id 字段是否存在，并且是 string 类型
+	if field := tx.Statement.ReflectValue.FieldByName("team_id"); field.IsValid() && field.CanSet() {
+		// 设置生成的唯一 ID
+		field.SetString(node.Generate().String())
+	}
+}
+
+func RegisterNode(db *gorm.DB) {
+	db.Callback().Create().Before("gorm:Create").Register("before_create_Node", BeforeCreateNode)
+}
+
+// 在操作数据库前 创建 节点 ID
+func BeforeCreateNode(tx *gorm.DB) {
+	node, err := snowflake.NewNode(Default_Node_ID)
+	if err != nil {
+		zlog.Fatalf("生成 Node 出错")
+	}
+
+	tx.Statement.ReflectValue = reflect.Indirect(tx.Statement.ReflectValue)
+	// 确认 myself_id 字段是否存在，并且是 string 类型
+	if field := tx.Statement.ReflectValue.FieldByName("myself_id"); field.IsValid() && field.CanSet() {
 		// 设置生成的唯一 ID
 		field.SetString(node.Generate().String())
 	}
