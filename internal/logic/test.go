3bb86020705486deae7c47c4092daf868c751c64
Dbinggo <djy1349678887@163.com>
2024-11-18 01:45:20 +0800

    feat: test logic做一下优化

--- internal/logic/test.go
+++ internal/logic/test.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"gorm.io/gorm"
 	"tgwp/internal/repo"
 	"tgwp/internal/response"
 	"tgwp/internal/types"
@@ -20,6 +22,7 @@ func NewTestLogic() *TestLogic {
 // 这里定义我们内部logic的错误（非公有的常见类型的错误）
 var (
 	codeUserFoundField = response.MsgCode{Code: 40013, Msg: "用户查询失败"}
+	codeUserNotFound   = response.MsgCode{Code: 40014, Msg: "用户不存在"}
 )
 
 // TestLogic 逻辑层 用做逻辑处理相关操作
@@ -29,9 +32,15 @@ func (l *TestLogic) TestLogic(ctx context.Context, req types.TestO1Req) (resp *t
 
 	user, err := repo.NewTestRepo().GetUserById(req.UserID)
 	if err != nil {
-		zlog.Errorf("get user error: %v", err)
-		//注意 codeUserFoundField 只是事例，具体根据实际情况定义 response.ErrResp这里用作包装错误和响应，使得错误更加通用
-		return nil, response.ErrResp(err, codeUserFoundField)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			zlog.CtxWarnf(ctx, "user not found: %v", err)
+			return nil, response.ErrResp(err, codeUserNotFound)
+		} else {
+			zlog.Errorf("get user error: %v", err)
+			//注意 codeUserFoundField 只是事例，具体根据实际情况定义 response.ErrResp这里用作包装错误和响应，使得错误更加通用
+			return nil, response.ErrResp(err, codeUserFoundField)
+		}
+
 	}
 	resp.Name = user.Name
 	resp.Age = user.Age
