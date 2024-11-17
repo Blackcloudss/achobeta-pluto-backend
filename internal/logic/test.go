e4be90c66b33d1855062ed89251faeadaad7cb9c
Dbinggo <djy1349678887@163.com>
2024-11-18 01:38:26 +0800

    feat: response对于err的进一步封装处理

--- internal/logic/test.go
+++ internal/logic/test.go
@@ -3,6 +3,7 @@ package logic
 import (
 	"context"
 	"tgwp/internal/repo"
+	"tgwp/internal/response"
 	"tgwp/internal/types"
 	"tgwp/log/zlog"
 	"tgwp/util"
@@ -16,16 +17,24 @@ func NewTestLogic() *TestLogic {
 	return &TestLogic{}
 }
 
+// 这里定义我们内部logic的错误（非公有的常见类型的错误）
+var (
+	codeUserFoundField = response.MsgCode{Code: 40013, Msg: "用户查询失败"}
+)
+
 // TestLogic 逻辑层 用做逻辑处理相关操作
-func (l *TestLogic) TestLogic(ctx context.Context, req types.TestO1Req) (resp types.Test01Resp, err error) {
+func (l *TestLogic) TestLogic(ctx context.Context, req types.TestO1Req) (resp *types.Test01Resp, err error) {
 	defer util.RecordTime(time.Now())()
 	//..... some logic
 
 	user, err := repo.NewTestRepo().GetUserById(req.UserID)
 	if err != nil {
-		zlog.CtxErrorf(ctx, "%v", err)
+		zlog.Errorf("get user error: %v", err)
+		//注意 codeUserFoundField 只是事例，具体根据实际情况定义 response.ErrResp这里用作包装错误和响应，使得错误更加通用
+		return nil, response.ErrResp(err, codeUserFoundField)
 	}
 	resp.Name = user.Name
 	resp.Age = user.Age
+
 	return
 }
