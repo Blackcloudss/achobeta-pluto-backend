27d8b8c687d478260b13ee0612bcadb3fa4edd41
Goblin-master <2896151192@qq.com>
2024-11-27 00:31:30 +0800

    fix: 美化路由

--- internal/router/web.go
+++ internal/router/web.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"tgwp/configs"
-	"tgwp/global"
 	"tgwp/internal/api"
 	"tgwp/internal/manager"
 	"tgwp/internal/middleware"
-	"tgwp/internal/response"
 	"tgwp/log/zlog"
 )
 
@@ -25,16 +23,12 @@ func RunServer() {
 // listen 配置 Gin 服务器
 func listen() (*gin.Engine, error) {
 	r := gin.Default() // 创建默认的 Gin 引擎
-
 	// 注册全局中间件（例如获取 Trace ID）
 	manager.RequestGlobalMiddleware(r)
-
 	// 创建 RouteManager 实例
 	routeManager := manager.NewRouteManager(r)
-
 	// 注册各业务路由组的具体路由
 	registerRoutes(routeManager)
-
 	return r, nil
 }
 
@@ -46,17 +40,10 @@ func registerRoutes(routeManager *manager.RouteManager) {
 	})
 	// 登录相关路由
 	routeManager.RegisterLoginRoutes(func(rg *gin.RouterGroup) {
-		rg.POST("/login", api.LoginWithCode)
+		//发送验证码
 		rg.POST("/code", api.GetCode)
-		rg.GET("/test", middleware.ReflashAtoken(), func(c *gin.Context) {
-			if token, exists := c.Get(global.AUTH_ENUMS_ATOKEN); exists {
-				response.NewResponse(c).Success(token)
-			}
-			//告诉后面的人如何拿到token里面的数据
-			if data, exists := c.Get(global.TOKEN_USER_ID); exists {
-				response.NewResponse(c).Success(data)
-			}
-		})
+		//用验证码登录
+		rg.POST("/login", api.LoginWithCode)
 		//是否可以自动登录
 		rg.POST("/auto", api.CheckAutoLogin)
 		//用户自行退出登录
@@ -65,6 +52,7 @@ func registerRoutes(routeManager *manager.RouteManager) {
 
 	// 展示常用设备页面相关操作路由
 	routeManager.RegisterDevicesRoutes(func(rg *gin.RouterGroup) {
+		//使用token验证中间件
 		rg.Use(middleware.ReflashAtoken())
 		//移除常用设备
 		rg.DELETE("/remove", api.RemoveDevice)
@@ -75,20 +63,16 @@ func registerRoutes(routeManager *manager.RouteManager) {
 	})
 	// 个人信息相关路由
 	routeManager.RegisterProfileRoutes(func(rg *gin.RouterGroup) {
-
-		// example
-		rg.GET("/info", func(c *gin.Context) {
-
-		})
+		//测试
 		rg.GET("test", api.Test)
 	})
 
 	// 团队信息相关路由
 	routeManager.RegisterTeamRoutes(func(rg *gin.RouterGroup) {
-
+		//使用token验证中间件
+		rg.Use(middleware.ReflashAtoken())
 		//获得权限组
 		rg.GET("/power", api.GetPower)
-
 		// 团队架构管理子路由
 		TeamStructure := rg.Group("/structure")
 		{
@@ -107,10 +91,12 @@ func registerRoutes(routeManager *manager.RouteManager) {
 		{
 			//查询团队内成员列表--成员简单信息
 			MemberList.GET("/get", api.GetTeamMemberlist)
+			//使用权限校验中间件
+			MemberList.Use(middleware.PermissionMiddleware())
 			//新增成员
-			MemberList.POST("/create", middleware.PermissionMiddleware(), api.CreateTeamMember)
+			MemberList.POST("/create", api.CreateTeamMember)
 			//删除成员                     // user_id 之后要删除
-			MemberList.DELETE("/delete/:user_id/:team_id/:member_id", middleware.PermissionMiddleware(), api.DeleteTeamMember)
+			MemberList.DELETE("/delete/:user_id/:team_id/:member_id", api.DeleteTeamMember)
 		}
 
 		// 团队成员信息管理子路由
@@ -120,8 +106,10 @@ func registerRoutes(routeManager *manager.RouteManager) {
 			MemberMsg.GET("/details", api.GetMemberDetail)
 			//给成员点赞/取消赞
 			MemberMsg.PUT("/like", api.PutLikeCount)
+			//使用权限校验中间件
+			MemberMsg.Use(middleware.PermissionMiddleware())
 			//编辑成员信息
-			MemberMsg.PUT("/change", middleware.PermissionMiddleware(), api.PutTeamMember)
+			MemberMsg.PUT("/change", api.PutTeamMember)
 		}
 	})
 
@@ -129,27 +117,33 @@ func registerRoutes(routeManager *manager.RouteManager) {
 	routeManager.RegisterMessageRoutes(func(rg *gin.RouterGroup) {
 		// 建立消息源
 		rg.POST("/set", api.SetMessage)
-		// 将消息与用户绑定
-		rg.POST("/join", middleware.ReflashAtoken(), api.JoinMessage)
-		// 获取消息列表(分页)
-		rg.GET("/get", middleware.ReflashAtoken(), api.GetMessage)
 		// 标记已读
 		rg.POST("/markread", api.MarkReadMessage)
+		//使用token验证中间件
+		rg.Use(middleware.ReflashAtoken())
+		// 将消息与用户绑定
+		rg.POST("/join", api.JoinMessage)
+		// 获取消息列表(分页)
+		rg.GET("/get", api.GetMessage)
 		// 一键发送消息
-		rg.POST("/send", middleware.ReflashAtoken(), api.SendMessage)
+		rg.POST("/send", api.SendMessage)
 	})
 
 	// 飞书相关路由组
 	routeManager.RegisterFeiShuRoutes(func(rg *gin.RouterGroup) {
+		//使用token验证中间件
+		rg.Use(middleware.ReflashAtoken())
 		// 获取飞书二维表格
-		rg.GET("/get", middleware.ReflashAtoken(), api.GetFeiShuList)
+		rg.GET("/get", api.GetFeiShuList)
 	})
 
 	// 个人中心相关路由组
 	routeManager.RegisterUserProfileRoutes(func(rg *gin.RouterGroup) {
+		//使用token验证中间件
+		rg.Use(middleware.ReflashAtoken())
 		//查询成员详细信息
-		rg.GET("/details", middleware.ReflashAtoken(), api.GetUserDetail)
+		rg.GET("/details", api.GetUserDetail)
 		//给成员点赞/取消赞
-		rg.PUT("/like", middleware.ReflashAtoken(), api.PutUserLikeCount)
+		rg.PUT("/like", api.PutUserLikeCount)
 	})
 }
